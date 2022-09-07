75d904f7b3ce2fe618c6ec8da129ad9bd8ad97c9
JinkwonHeo <hjinkwon912@gmail.com>
2022-09-07 18:18:22 +0900

    lecture2: path

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { select } from 'd3';
+import { select, line, curveCardinal } from 'd3';
 
 function App() {
-  const [data, setData] = useState([25, 30, 45, 60, 20]);
+  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
   const svgRef = useRef<SVGSVGElement>(null);
 
   useEffect(() => {
     const svg = select(svgRef.current);
+    const myLine = line<number>()
+      .x((data, index) => index * 50)
+      .y((value) => 150 - value)
+      .curve(curveCardinal);
+
     svg
-      .selectAll('circle')
-      .data(data)
-      .join('circle')
-      .attr('r', (value) => value)
-      .attr('cx', (value) => value * 2)
-      .attr('cy', (value) => value * 2)
-      .attr('stroke', 'red');
+      .selectAll('path')
+      .data([data])
+      .join('path')
+      .attr('d', (value) => myLine(value))
+      .attr('fill', 'none')
+      .attr('stroke', 'blue');
+    // svg
+    //   .selectAll('circle')
+    //   .data(data)
+    //   .join('circle')
+    //   .attr('r', (value) => value)
+    //   .attr('cx', (value) => value * 2)
+    //   .attr('cy', (value) => value * 2)
+    //   .attr('stroke', 'red');
   }, [data]);
   return (
     <>
